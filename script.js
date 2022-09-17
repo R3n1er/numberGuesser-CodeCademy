4eb8d4b934d4c921c33375ed3658d99b3db316f7
Alban RENIER <renier.alban@gmail.com>
2022-09-17 10:42:21 -0300

    creation de la fonction compare guess

--- script.js
+++ script.js
@@ -12,3 +12,26 @@ The purpose of this function is to be called at the start of each new round in o
 const generateTarget = () => {
   return Math.floor(Math.random() * 10); // générer un nombre aléatoire avec Math.random
 };
+
+/*
+Create a compareGuesses() function. This function:
+
+- Has three parameters representing the user (human) guess, a computer guess, and the secret target number to be guessed.
+- Determines which player (human or computer) wins based on which guess is closest to the target. If both players are tied, the human user should win.
+- Return true if the human player wins, and false if the computer player wins.
+
+The purpose of this function is to be called each round to determine which guess is closest to the target number.
+*/
+
+const compareGuesses = (humanGuess, computerGuess, targetGuess) => {
+  const humanDifference = Math.abs(targetGuess - humanGuess); // Math.abs() retourne la valeur absolue d'un nombre
+  const computerDifference = Math.abs(targetGuess - computerGuess);
+  return humanDifference <= computerDifference;
+};
+
+/*
+
+
+
+
+*/
