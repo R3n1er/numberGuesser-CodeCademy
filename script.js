04392e1ac1dddde92ddceec0f7a447d8b56e9821
Alban RENIER <renier.alban@gmail.com>
2022-09-17 10:37:17 -0300

    creation de la fonction generateTarget

--- script.js
+++ script.js
@@ -9,3 +9,6 @@ Create a generateTarget() function. This function should return a random integer
 
 The purpose of this function is to be called at the start of each new round in order to generate the new secret target number.  
  */
+const generateTarget = () => {
+  return Math.floor(Math.random() * 10); // générer un nombre aléatoire avec Math.random
+};
