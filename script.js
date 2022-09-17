981350aa1caa381bcb5b1cc43c6b0ef635a9d2ef
Alban RENIER <renier.alban@gmail.com>
2022-09-17 10:33:59 -0300

    initialisation du projet

---
+++ script.js
@@ -0,0 +1,11 @@
+let humanScore = 0;
+let computerScore = 0;
+let currentRoundNumber = 1;
+
+// Write your code below:
+
+/**
+Create a generateTarget() function. This function should return a random integer between 0 and 9.
+
+The purpose of this function is to be called at the start of each new round in order to generate the new secret target number.  
+ */
